3274ca2d344cf3c09175e0a16c4815fe3b1f51f7
acikmese <yasinacikmese@gmail.com>
2022-04-26 01:54:23 +0300

    update jetson nano script to handle all zip operation add selected model path remove unnecessary codes

--- jetson_run.py
+++ jetson_run.py
@@ -2,8 +2,10 @@ import cv2
 import argparse
 import os
 import sys
+import shutil
 from pathlib import Path
 from datetime import datetime, timezone
+from copy import deepcopy
 
 import torch
 import torch.backends.cudnn as cudnn
@@ -60,15 +62,12 @@ def run(weights=YOLO_ROOT / 'yolov5s.pt',  # model.pt path(s)
         ):
     source = str(source)
     save_img = True
-    save_interval = 1  # in seconds
-
-    save_dir = path_with_date(Path(project) / name)  # output with date
-    images_dir = save_dir.joinpath('images')
-
-    if not nosave:
-        (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
-        if save_img:
-            images_dir.mkdir(parents=True, exist_ok=True)
+    zip_files = True
+    annotate_img = True
+    zip_files_interval = 1 * 10  # in seconds
+    img_save_interval = 1  # in seconds
+    zip_txt_dir = ROOT / 'zip_txt_final'
+    zip_img_dir = ROOT / 'zip_img_final'
 
     # Load model
     device = select_device(device)
@@ -79,14 +78,12 @@ def run(weights=YOLO_ROOT / 'yolov5s.pt',  # model.pt path(s)
     # Dataloader
     view_img = check_imshow() if view_img else False
     cudnn.benchmark = True  # set True to speed up constant image size inference
-    dataset = LoadCSI(source, img_size=imgsz, stride=stride, auto=pt)
-    # dataset = LoadWebcam(source, img_size=imgsz, stride=stride, auto=pt)
+    # dataset = LoadCSI(source, img_size=imgsz, stride=stride, auto=pt)
+    dataset = LoadWebcam(source, img_size=imgsz, stride=stride, auto=pt)
     bs = len(dataset)  # batch_size
 
     # Focal length calibration.
     focal = None
-
-    vid_path, vid_writer = [None] * bs, [None] * bs
 
     utc_prev_time = datetime.now(timezone.utc)
 
@@ -94,11 +91,46 @@ def run(weights=YOLO_ROOT / 'yolov5s.pt',  # model.pt path(s)
     model.warmup(imgsz=(1 if pt else bs, 3, *imgsz))  # warmup
     dt, seen = [0.0, 0.0, 0.0], 0
 
+    first_run = True  # if first run, create directories for output
+    new_dir_created = False  # if new directory created, it will zip folder and move to output path
+
+    zip_timer = datetime.now()
+
+    if zip_files:
+        txt_zip_dir = Path(project) / name / 'tmp_txt_zips'
+        txt_zip_dir.mkdir(parents=True, exist_ok=True)
+        if save_img:
+            img_zip_dir = Path(project) / name / 'tmp_img_zips'
+            img_zip_dir.mkdir(parents=True, exist_ok=True)
+
     for path, im, im0s, vid_cap, s in dataset:
         t1 = time_sync()
         # Get datetime of current image.
         utc_time = datetime.now(timezone.utc)
         utc_iso = utc_time.isoformat()
+
+        # Generate directory
+        zip_timer_diff = (datetime.now() - zip_timer).total_seconds()
+
+        if (not nosave) and ((zip_timer_diff >= zip_files_interval) or first_run):
+            save_dir, dir_name = path_with_date(Path(project) / name, utc_time)  # output with date
+            txt_dir = save_dir.joinpath('txt')
+            img_dir = save_dir.joinpath('images')
+
+            (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+            txt_dir.mkdir(parents=True, exist_ok=True)
+            if save_img:
+                img_dir.mkdir(parents=True, exist_ok=True)
+
+            if not first_run:
+                new_dir_created = True
+            else:
+                first_run = False
+                old_save_dir = deepcopy(save_dir)
+                old_txt_dir = deepcopy(txt_dir)
+                old_img_dir = deepcopy(img_dir)
+                old_dir_name = deepcopy(dir_name)
+
         im = torch.from_numpy(im).to(device)
         im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
         im /= 255  # 0 - 255 to 0.0 - 1.0
@@ -137,8 +169,6 @@ def run(weights=YOLO_ROOT / 'yolov5s.pt',  # model.pt path(s)
                     focal = focal_length
 
             p = Path(p)  # to Path
-            save_path = str(save_dir / p.name)  # im.jpg
-            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt
             s += '%gx%g ' % im.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             annotator = Annotator(im0, line_width=line_thickness, example=str(names))
@@ -159,13 +189,7 @@ def run(weights=YOLO_ROOT / 'yolov5s.pt',  # model.pt path(s)
                     obj_height = xyxy[3] - xyxy[1]  # Calculate length of height of bounding box of detected object
                     d = calculate_distance(obj_height, focal, avg_height, cls)
 
-                    if save_txt:  # Write to file
-                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
-                        with open(txt_path + '.txt', 'a') as f:
-                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
-
-                    if view_img:  # Add bbox to image
+                    if view_img or annotate_img:  # Add bbox to image
                         c = int(cls)  # integer class
                         lbl = f"{names[c]} | {conf:.2f} | {d[0]:.1f}m" if add_distance else f"{names[c]} | {conf:.2f}"
                         label = None if hide_labels else (names[c] if hide_conf else lbl)
@@ -179,7 +203,7 @@ def run(weights=YOLO_ROOT / 'yolov5s.pt',  # model.pt path(s)
                     label = names[c]
                     confidence = float(conf)
                     line = [utc_iso, frame, label, c, confidence, *xywh, *xywh_norm, *d]  # label format
-                    prod_text_path = str(save_dir / p.stem) + ".txt"
+                    prod_text_path = str(txt_dir / p.stem) + ".txt"
                     if not os.path.isfile(prod_text_path) or os.stat(prod_text_path).st_size == 0:
                         with open(prod_text_path, 'a') as f:
                             out_txt = f"datetime_utc,frame_id,id_type,id,confidence,x_center,y_center,x_width,y_width,"\
@@ -196,31 +220,32 @@ def run(weights=YOLO_ROOT / 'yolov5s.pt',  # model.pt path(s)
                 cv2.imshow(str(p), im0)
                 cv2.waitKey(1)  # 1 millisecond
 
-            # # Save results (image with detections)
-            # if save_img:
-            #     if dataset.mode == 'image':
-            #         cv2.imwrite(save_path, im0)
-            #     else:  # 'video' or 'stream'
-            #         if vid_path[i] != save_path:  # new video
-            #             vid_path[i] = save_path
-            #             if isinstance(vid_writer[i], cv2.VideoWriter):
-            #                 vid_writer[i].release()  # release previous video writer
-            #             if vid_cap:  # video
-            #                 fps = vid_cap.get(cv2.CAP_PROP_FPS)
-            #                 w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-            #                 h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-            #             else:  # stream
-            #                 fps, w, h = 30, im0.shape[1], im0.shape[0]
-            #             save_path = str(Path(save_path).with_suffix('.mp4'))  # force *.mp4 suffix on results videos
-            #             vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
-            #         vid_writer[i].write(im0)
-
-            if save_img and (utc_time - utc_prev_time).total_seconds() >= save_interval:
-                tf = utc_time
-                f_name = f"{p.stem}_{tf.year}_{tf.month}_{tf.day}_{tf.hour}_{tf.minute}_{tf.second}"
-                img_path_name = str(images_dir) + '/' + f_name + '.jpg'
-                cv2.imwrite(img_path_name, im0, [cv2.IMWRITE_JPEG_QUALITY, 90])
+            if save_img and (utc_time - utc_prev_time).total_seconds() >= img_save_interval:
+                it = utc_time
+                f_name = f"{p.stem}_{it.year}_{it.month}_{it.day}_{it.hour}_{it.minute}_{it.second}"
+                img_path_name = str(img_dir) + '/' + f_name + '.jpg'
+                cv2.imwrite(img_path_name, im0, [cv2.IMWRITE_JPEG_QUALITY, 80])
                 utc_prev_time = utc_time
+
+        if zip_files and new_dir_created:
+            zip_txt_path = txt_zip_dir / old_dir_name
+            shutil.move(old_txt_dir, zip_txt_path)
+            shutil.make_archive(zip_txt_path, 'zip', zip_txt_path)
+            shutil.rmtree(zip_txt_path, ignore_errors=True)
+            shutil.move(str(zip_txt_path) + ".zip", str(zip_txt_dir / old_dir_name) + ".zip")
+            if save_img:
+                zip_img_path = img_zip_dir / old_dir_name
+                shutil.move(old_img_dir, zip_img_path)
+                shutil.make_archive(zip_img_path, 'zip', zip_img_path)
+                shutil.rmtree(zip_img_path, ignore_errors=True)
+                shutil.move(str(zip_img_path) + ".zip", str(zip_img_dir / old_dir_name) + ".zip")
+            shutil.rmtree(old_save_dir, ignore_errors=True)
+            old_dir_name = dir_name
+            old_save_dir = save_dir
+            old_txt_dir = txt_dir
+            old_img_dir = img_dir
+            new_dir_created = False
+            zip_timer = datetime.now()
 
         # Print time (inference-only)
         LOGGER.info(f'{s}Done. ({t3 - t2:.3f}s)')
@@ -241,14 +266,15 @@ def parse_opt():
     parser.add_argument('--source', type=str, default=ROOT / 'streams.txt', help='file/dir/URL/glob, 0 for webcam')
     parser.add_argument('--data', type=str, default=YOLO_ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
-    parser.add_argument('--conf-thres', type=float, default=0.4, help='confidence threshold')
+    parser.add_argument('--conf-thres', type=float, default=0.3, help='confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
     parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--view-img', action='store_true', help='show results')
+    parser.add_argument('--save-txt', action='store_true', help='save texts')
     parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
     parser.add_argument('--classes', nargs='+', type=int, default=[0, 2, 5, 7], help='filter by class: --classes 0 2')
-    parser.add_argument('--name', default='dets', help='save results to project/name')
+    parser.add_argument('--name', default='ff', help='save results to project/name')
     parser.add_argument('--focal-length', default=1000, type=int, help='focal length of camera in pixels')
     parser.add_argument('--avg-height', default=1.75, type=float, help='average height of human in meters')
     opt = parser.parse_args()
