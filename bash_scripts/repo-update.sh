f51002de4c013b4af9563ae8dc51ad4940af1b42
Yasin Acikmese <yasinacikmese@gmail.com>
2022-05-10 10:22:35 +0300

    update object detection repo update script

--- bash_scripts/repo-update.sh
+++ bash_scripts/repo-update.sh
@@ -3,12 +3,12 @@ echo "Updating Repo!"
 adb pull /storage/emulated/0/fireplay/camera-project/repo/jetson-object-detection.zip /home/ff/Desktop/new_repo/
 REPO=/home/ff/Desktop/new_repo/jetson-object-detection.zip
 if [ -f "$REPO" ]; then
-	# TODO: Add here stop the service of framework.
+	sudo systemctl stop firefly-object-detection.service
     unzip /home/ff/Desktop/new_repo/jetson-object-detection.zip -d /home/ff/Desktop/new_repo/
     rsync -az /home/ff/Desktop/new_repo/jetson-object-detection/ /home/ff/Desktop/jetson-object-detection/
     rm -rf /home/ff/Desktop/new_repo/jetson-object-detection
     rm -rf /home/ff/Desktop/new_repo/jetson-object-detection.zip
-    # TODO: Add here to start the service again.
+    sudo systemctl start firefly-object-detection.service
 fi
 
 exit 0
