bc701329851eaee7c8b0b9a380270b1d160e1a92
Yasin Acikmese <yasinacikmese@gmail.com>
2022-05-17 23:59:53 +0300

    check running object detection service

---
+++ bash_scripts/check-running-service.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SERVICE="firefly-object-detection.service"
+STATUS="$(systemctl is-active firefly-object-detection.service)"
+
+# Check if service is active
+if [ "${STATUS}" = "active" ]; then
+    echo "$SERVICE is running!"
+else 
+    echo " Service not running, restarting...! "  
+    # Restart camera daemon
+    echo 1324 | sudo -S systemctl restart nvargus-daemon
+    # Restart object detection service
+    echo 1324 | sudo -S systemctl start firefly-object-detection.service
+fi
