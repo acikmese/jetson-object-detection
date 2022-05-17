2870bb3e43ec80ce6963711950cab69ba8f8d46b
Yasin Acikmese <yasinacikmese@gmail.com>
2022-05-17 23:32:57 +0300

    fix transferring outputs to display path

--- bash_scripts/transfer-outputs-to-display.sh
+++ bash_scripts/transfer-outputs-to-display.sh
@@ -2,17 +2,17 @@
 
 # Transfer output data
 echo "Transferring output data!"
-adb push /home/ff/Desktop/jetson-object-detection/zipped_data/* /storage/emulated/0/fireplay/camera-project/data
-sudo rm -rf /home/ff/Desktop/jetson-object-detection/zipped_data/*
+adb push /home/ff/Desktop/jetson-object-detection/zipped_data/*.zip /storage/emulated/0/fireplay/camera-project/data
+sudo rm -rf /home/ff/Desktop/jetson-object-detection/zipped_data/*.zip
 
 # Transfer logs
 echo "Transferring logs!"
-adb push /home/ff/Desktop/jetson-object-detection/zipped_logs/* /storage/emulated/0/fireplay/camera-project/logs
-sudo rm -rf /home/ff/Desktop/jetson-object-detection/zipped_logs/*
+adb push /home/ff/Desktop/jetson-object-detection/zipped_logs/*.zip /storage/emulated/0/fireplay/camera-project/logs
+sudo rm -rf /home/ff/Desktop/jetson-object-detection/zipped_logs/*.zip
 
 # Transfer images data
 echo "Transferring images!"
-adb push /home/ff/Desktop/jetson-object-detection/zipped_images/* /storage/emulated/0/fireplay/camera-project/image
-sudo rm -rf /home/ff/Desktop/jetson-object-detection/zipped_images/*
+adb push /home/ff/Desktop/jetson-object-detection/zipped_images/*.zip /storage/emulated/0/fireplay/camera-project/images
+sudo rm -rf /home/ff/Desktop/jetson-object-detection/zipped_images/*.zip
 
 exit 0
