1bdfe2abd011b0929a84f788cd2940456d5636f8
Yasin Acikmese <yasinacikmese@gmail.com>
2022-05-18 00:25:05 +0300

    update cron job bash file

--- bash_scripts/jetson-cron-job.sh
+++ bash_scripts/jetson-cron-job.sh
@@ -2,10 +2,12 @@
 
 # 1. Transfer files to display path.
 echo 1324 | sudo -S bash /home/ff/Desktop/jetson-object-detection/bash_scripts/transfer-outputs-to-display.sh
-echo "Zipped files are transferred!"
+echo "Transferring files are done!"
 
 # 2. Check repo update and do the update.
 echo 1324 | sudo -S bash /home/ff/Desktop/jetson-object-detection/bash_scripts/repo_update.sh
+echo "Repo update is done!"
 
 # 3. Check if service is running, if not, restart.
 echo 1324 | sudo -S bash /home/ff/Desktop/jetson-object-detection/bash_scripts/check-running-service.sh
+echo "Service check is done!"
