120ef06cdb2c84ee770df2e74ffe599f2506d85d
ff <ff@firefly-jetson.home>
2022-05-24 02:11:33 -0400

    add removing old files, update cron job

--- bash_scripts/jetson-cron-job.sh
+++ bash_scripts/jetson-cron-job.sh
@@ -5,9 +5,13 @@ echo 1324 | sudo -S bash /home/ff/jetson-object-detection/bash_scripts/transfer-
 echo "Transferring files are done!"
 
 # 2. Check repo update and do the update.
-echo 1324 | sudo -S bash /home/ff/jetson-object-detection/bash_scripts/repo_update.sh
-echo "Repo update is done!"
+#echo 1324 | sudo -S bash /home/ff/jetson-object-detection/bash_scripts/repo_update.sh
+#echo "Repo update is done!"
 
 # 3. Check if service is running, if not, restart.
 echo 1324 | sudo -S bash /home/ff/jetson-object-detection/bash_scripts/check-running-service.sh
 echo "Service check is done!"
+
+# 4. Remove old files if necessary.
+echo 1324 | sudo -S bash /home/ff/jetson-object-detection/bash_scripts/remove-old-files.sh
+echo "Old files are removed!"
