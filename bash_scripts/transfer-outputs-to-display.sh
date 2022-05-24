ccc7a45655fc6369d27427214d783093dd2597d5
acikmese <yasinacikmese@gmail.com>
2022-05-24 15:25:23 +0300

    Update transfer-to-display script with new paths

--- bash_scripts/transfer-outputs-to-display.sh
+++ bash_scripts/transfer-outputs-to-display.sh
@@ -2,17 +2,17 @@
 
 # Transfer output data
 echo "Transferring output data!"
-adb push /home/ff/jetson-object-detection/zipped_data/*.zip /storage/emulated/0/fireplay/camera-project/data
+adb push /home/ff/jetson-object-detection/zipped_data/*.zip /storage/emulated/0/fireplay/camera/data
 sudo rm -rf /home/ff/jetson-object-detection/zipped_data/*.zip
 
 # Transfer logs
 echo "Transferring logs!"
-adb push /home/ff/jetson-object-detection/zipped_logs/*.zip /storage/emulated/0/fireplay/camera-project/logs
+adb push /home/ff/jetson-object-detection/zipped_logs/*.zip /storage/emulated/0/fireplay/camera/log
 sudo rm -rf /home/ff/jetson-object-detection/zipped_logs/*.zip
 
 # Transfer images data
 echo "Transferring images!"
-adb push /home/ff/jetson-object-detection/zipped_images/*.zip /storage/emulated/0/fireplay/camera-project/images
+adb push /home/ff/jetson-object-detection/zipped_images/*.zip /storage/emulated/0/fireplay/camera/picture
 sudo rm -rf /home/ff/jetson-object-detection/zipped_images/*.zip
 
 exit 0
