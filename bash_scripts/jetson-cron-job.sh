e93074dbfb351973dcbaf967ab5173274e2a35c6
ff <ff@firefly-jetson.home>
2022-05-31 17:10:03 -0400

    add repo update script

--- bash_scripts/jetson-cron-job.sh
+++ bash_scripts/jetson-cron-job.sh
@@ -5,8 +5,8 @@ echo 1324 | sudo -S bash /home/ff/jetson-object-detection/bash_scripts/transfer-
 echo "Transferring files are done!"
 
 # 2. Check repo update and do the update.
-#echo 1324 | sudo -S bash /home/ff/jetson-object-detection/bash_scripts/repo_update.sh
-#echo "Repo update is done!"
+echo 1324 | sudo -S bash /home/ff/jetson-object-detection/bash_scripts/repo_update.sh
+echo "Repo update is done!"
 
 # 3. Check if service is running, if not, restart.
 echo 1324 | sudo -S bash /home/ff/jetson-object-detection/bash_scripts/check-running-service.sh
