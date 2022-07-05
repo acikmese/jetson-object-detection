dde49c81c54042a5c32982ea12eec0d569d4e52a
acikmese <yasinacikmese@gmail.com>
2022-07-05 16:54:58 +0300

    update time range for removing old data

--- bash_scripts/remove-old-files.sh
+++ bash_scripts/remove-old-files.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
+# Found in following link.
+# https://unix.stackexchange.com/a/205539
+
 # Remove old zipped data.
-# cd /home/ff/jetson-object-detection/zipped_data/
-# ls *.zip -t | sed -e '1,500d' | xargs -d '\n' rm
-find /home/ff/jetson-object-detection/zipped_data/ -mindepth 1 -type f -mmin +25 -delete
+find /home/ff/jetson-object-detection/zipped_data/ -mindepth 1 -type f -mtime +2 -delete
 
 # Remove old zipped images.
-# cd /home/ff/jetson-object-detection/zipped_images/
-# ls *.zip -t | sed -e '1,100d' | xargs -d '\n' rm
-find /home/ff/jetson-object-detection/zipped_images/ -mindepth 1 -type f -mmin +25 -delete
+find /home/ff/jetson-object-detection/zipped_images/ -mindepth 1 -type f -mmin +720 -delete
 
 # Remove old zipped logs.
-# cd /home/ff/jetson-object-detection/zipped_logs/
-# ls *.zip -t | sed -e '1,500d' | xargs -d '\n' rm
-find /home/ff/jetson-object-detection/zipped_logs/ -mindepth 1 -type f -mmin +25 -delete
+find /home/ff/jetson-object-detection/zipped_logs/ -mindepth 1 -type f -mtime +2 -delete
