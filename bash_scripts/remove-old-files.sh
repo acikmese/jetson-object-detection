79da1894d83a59319ab066411c8d1d22bd465fc7
acikmese <yasinacikmese@gmail.com>
2022-07-05 16:51:48 +0300

    update remove old files

--- bash_scripts/remove-old-files.sh
+++ bash_scripts/remove-old-files.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Remove old zipped data.
-cd /home/ff/jetson-object-detection/zipped_data/
-ls *.zip -t | sed -e '1,500d' | xargs -d '\n' rm
+# cd /home/ff/jetson-object-detection/zipped_data/
+# ls *.zip -t | sed -e '1,500d' | xargs -d '\n' rm
+find /home/ff/jetson-object-detection/zipped_data/ -mindepth 1 -type f -mmin +25 -delete
 
 # Remove old zipped images.
-cd /home/ff/jetson-object-detection/zipped_images/
-ls *.zip -t | sed -e '1,100d' | xargs -d '\n' rm
+# cd /home/ff/jetson-object-detection/zipped_images/
+# ls *.zip -t | sed -e '1,100d' | xargs -d '\n' rm
+find /home/ff/jetson-object-detection/zipped_images/ -mindepth 1 -type f -mmin +25 -delete
 
 # Remove old zipped logs.
-cd /home/ff/jetson-object-detection/zipped_logs/
-ls *.zip -t | sed -e '1,500d' | xargs -d '\n' rm
+# cd /home/ff/jetson-object-detection/zipped_logs/
+# ls *.zip -t | sed -e '1,500d' | xargs -d '\n' rm
+find /home/ff/jetson-object-detection/zipped_logs/ -mindepth 1 -type f -mmin +25 -delete
